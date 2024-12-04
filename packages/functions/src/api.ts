acd06d2758d4e5961921a4ca38cf43caff7f7cca
yasuwrldhyper <yhppfzgd@gmail.com>
2024-12-05 07:39:01 +0900

    feat(be): upload file to s3

--- packages/functions/src/api.ts
+++ packages/functions/src/api.ts
@@ -1,8 +1,23 @@
 import { Resource } from "sst";
 import { Handler } from "aws-lambda";
 import { Example } from "@monorepo-template/core/example";
+import { PutObjectCommand, S3Client } from "@aws-sdk/client-s3"
 
 export const handler: Handler = async (_event) => {
+
+  const s3 = new S3Client();
+
+  const data = {
+    date: new Date()
+  }
+
+  await s3.send(new PutObjectCommand({
+    Key: "sanple.json",
+    Body: JSON.stringify(data),
+    Bucket: Resource.MyBucket.name, // SST が自動注入
+    ContentType: "application/json"
+  }));
+
   return {
     statusCode: 200,
     body: `${Example.hello()} Linked to ${Resource.MyBucket.name}.`,
--- sst.config.ts
+++ sst.config.ts
@@ -3,7 +3,7 @@
 export default $config({
   app(input) {
     return {
-      name: "monorepo-template",
+      name: "sst-example",
       removal: input?.stage === "production" ? "retain" : "remove",
       home: "aws",
     };
